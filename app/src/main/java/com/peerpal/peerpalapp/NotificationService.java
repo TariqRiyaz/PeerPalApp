b843eb29f64a6c908f71875eeb4482a6fa1aa59e
Liamisthere <133641525+Liamisthere@users.noreply.github.com>
2024-05-24 10:14:17 +1200

    Added some additional code for NotificationService

--- app/src/main/java/com/peerpal/peerpalapp/NotificationService.java
+++ app/src/main/java/com/peerpal/peerpalapp/NotificationService.java
@@ -13,6 +13,17 @@ public class NotificationService extends FirebaseMessagingService {
 
 
 
+        //FileInputStream serviceAccount = new FileInputStream("path/to/your-service-account-file.json");
 
+       //FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).setDatabaseUrl("https://<YOUR_PROJECT_ID>.firebaseio.com/").build();
+       //FirebaseApp.initializeApp(options);
+
+       //String registrationToken = "YOUR_REGISTRATION_TOKEN";
+
+
+       //Message message = Message.builder().setNotification(new Notification("Test Notification", "This is a test notification")).setToken(registrationToken).build();
+
+    //String response = FirebaseMessaging.getInstance().send(message);
+    // System.out.println("Successfully sent message: " + response);
 
 }
